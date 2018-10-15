1ceeb384108a5a57d2f754b972643770ed2aad89
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-10-15 16:00:51 -0400

    vagrant/tests: use `ops http poll` to address "the waiting problem"
    that problem is: how long will it take to download and run the task?
    
    the time to do so varies from host to host and from time to time, and
    when we don't want to wait longer than we need to.. and we don't want to
    have a false positive saying the service is unhealthy when it just
    needed another few seconds to come online.. thus, we use an HTTP poll to
    confirm when we should proceed. We don't want that to poll forever, so
    we set 60 seconds as an upper limit. Update the comments while we're
    here.. we have the echo's to cover.

--- tests/scripts/test-nomad-job.sh
+++ tests/scripts/test-nomad-job.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
+set -ux
 
 # Fix error nomad can resolve nomad.service.consul domain
 ip=`ifconfig enp0s3 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`
-grep $ip /etc/hosts || echo "$ip nomad.service.consul" >> /etc/hosts
 
-# Run nomad job
-echo "Init example job"
+echo "Run example (hello world) job"
 nomad run /vagrant/tests/scripts/test-nomad.nomad
-echo "Wait 10s to nomad init the job"
-sleep 10
 
-# Check status of the job
+echo "Wait until the job starts (might take a moment to download the docker image)"
+ops http poll -r 60 $ip:8080
+
 echo "Example job status"
 nomad status example
 
-# Test nomad job
 echo "Curl to check that nomad job is up"
 stdbuf -oL curl -v $ip:8080
