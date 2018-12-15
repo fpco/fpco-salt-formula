7275f5949aa6e0d4ee813dbce8c235f7fab46fb3
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-12-15 10:45:25 -0500

    tests/create-vault-client-token: lookup VAULT_TOKEN from unseal JSON

--- tests/scripts/create-vault-client-token.sh
+++ tests/scripts/create-vault-client-token.sh
@@ -2,6 +2,7 @@
 
 set -ux
 
+VAULT_TOKEN=$(cat /vagrant/vault-unseal.json | jl '_.root_token' | tr -d \")
 # this token should match what is in nomad:vault:token key in pillar.
 # specifically, tests/srv/pillar/bootstrap.sls, and as a result of
 # applying the formula, also /etc/nomad/config.hcl in the VM
