bdab2cd2227d138f225efc52c28b036e02ed4979
ketzacoatl <ketzacoatl@users.noreply.github.com>
2018-12-26 21:13:09 -0500

    vagrant/box: move provisioning --> init shell script

---
+++ tests/scripts/vagrant-init/box.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# install python and related packages/tools
+salt-call --local state.sls python.pip --log-level=debug
+
+# install reclass and run basic setup/config
+# this will use bootstrap.sls to setup node/class mapping
+salt-call --local state.sls reclass --log-level=debug
+
+# this will use reclass config to control contents of top.sls
+# when creating a base box, we aim to install tools, not run them
+# the node/class mapping controls this, edit bootstrap pillar to customize
+#salt-call --local state.sls reclass.managed_tops --log-level=debug
+
+# using the updated top.sls, run highstate (all formula in node's top.sls)
+salt-call --local state.highstate --log-level=debug
+
+# clean up before exiting (we will create a box from this vm)
+apt-get clean
+dd if=/dev/zero of=/compact bs=1M
+rm -f /compact
+rm -rf /home/*/.ssh /root/.ssh/
+
+# parting message
+echo "DONE! repackage VM into box with:"
+echo "vagrant package --output my-cool.box"
+
+# clear shell history
+cat /dev/null > ~/.bash_history && history -c && exit
+ 
