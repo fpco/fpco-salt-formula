7e079b51478fff0f961291daf5d867e750d8877b
Ketzacoatl <ketzacoatl@users.noreply.github.com>
2019-04-06 15:45:54 -0400

    vagrant/box: make the dd error more obviously benign

--- tests/scripts/vagrant-init/box.sh
+++ tests/scripts/vagrant-init/box.sh
@@ -29,13 +29,23 @@ jq --version
 
 # clean up before exiting (we will create a box from this vm)
 apt-get clean
-dd if=/dev/zero of=/compact bs=1M
-rm -f /compact
 
 # this lets us easily set the hostname/minion id
 rm -f /etc/salt/minion_id
 
+echo "\n=================================================================\n"
+echo "This next step will use dd to fill the remaining empty space with"
+echo "zeros so it can be compacted out. This next step will look like a"
+echo "failure with an error that looks like the following:"
+echo "default: dd: error writing '/compact': No space left on device"
+echo "but that is expected, and is not an actual failure or error."
+echo "\n.................................................................\n"
+dd if=/dev/zero of=/compact bs=1M || true
+rm -f /compact
+echo "\n=================================================================\n"
+
 # parting message
+date
 echo "DONE! repackage VM into box with:"
 echo "vagrant package --output my-cool.box"
 
